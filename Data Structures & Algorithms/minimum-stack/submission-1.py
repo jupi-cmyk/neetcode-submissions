3053ba2459d2fcba60a574762259186698d73cd8
jupi-cmyk <anto.dappo97@gmail.com>
2026-06-28 12:04:50 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        last = self.stack[-1]
+        self.stack = self.stack[:-1]
+        return last
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return min(self.stack)
